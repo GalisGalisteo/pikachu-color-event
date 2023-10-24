c2886396ae3b614e6bf8304a3ef67fc5630bd7a7
GalisGalisteo <galis.travelling@gmail.com>
2023-10-24 12:01:32 +0200

    ex finished, 2 solutions

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Pika-pika suerte!
+
+const colorsNode = document.querySelector('#colors');
+const pikachuNode = document.querySelector('#pikachu');
+
+colorsNode.addEventListener('click', (e) => {
+    // option 1
+    // pikachuNode.style.background = e.target.id;
+    
+    // option 2
+    pikachuNode.style.background = window.getComputedStyle(e.target).backgroundColor;
+})
